55131efdff26be1f47ea8fa7d5e9c14468d0512b
CodyA1117 <adamscody079@gmail.com>
2024-11-08 14:30:42 -0600

    Test 1 passes.

--- queries.sql
+++ queries.sql
@@ -1,11 +1,11 @@
 --Part 1
---Columns in the 'job' table:
---id INT AUTO_INCREMENT PRIMARY KEY
---name VARCHAR(255)
---employer VARCHAR(255)
---location VARCHAR(255)
---position_type VARCHAR(255)
---core_competency VARCHAR(255)
+Columns in the 'job' table:
+id INT AUTO_INCREMENT PRIMARY KEY
+name VARCHAR(255)
+employer VARCHAR(255)
+location VARCHAR(255)
+position_type VARCHAR(255)
+core_competency VARCHAR(255)
 
 --Part 2
 
