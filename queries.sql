812e56bf71d99b8b049db304c01708e3b0276e47
CodyA1117 <adamscody079@gmail.com>
2024-11-11 19:37:20 -0600

    attempt to pass autograder on git hub, all tests passing in intelij.

--- queries.sql
+++ queries.sql
@@ -12,3 +12,6 @@ SELECT name FROM employer WHERE location = "St. Louis City";
 --Part 3
 DROP TABLE job;
 --Part 4
+SELECT * FROM skill
+JOIN job_skills ON skill.id = job_skills.skills_id
+ORDER BY name ASC;
